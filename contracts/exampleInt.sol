4d66de31253698aa47343ca9676d4c326c15f212
trisitian <tristian.labanowich@gmail.com>
2025-08-04 21:33:55 -0400

    playing around with primatives

--- contracts/exampleInt.sol
+++ contracts/exampleInt.sol
@@ -6,7 +6,30 @@ contract ExampleInt {
 
     uint public foo;
 
+    uint8 public myUint8;
+
+    int public myInt;
+
+    function incrementInt() private  {
+        myInt++;
+    }
+
+    function decrementInt() private {
+        myInt--;
+    }
+
+    function changeIntVal(bool toIncrement) public{
+        if(toIncrement == true){
+            incrementInt();
+        }else{
+            decrementInt();
+        }
+    }
     function setUint(uint _newVal) public {
         foo = _newVal;
+    }
+
+    function incrementUint9() public {
+        myUint8++;
     }
 }
